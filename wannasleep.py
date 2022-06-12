2e6240322d35716b9daa61bbc51de605f8d84229
Quimzy <bam.boy2020@gmail.com>
2022-06-12 12:04:18 +0300

    takes error from previous command

--- wannasleep.py
+++ wannasleep.py
@@ -3,11 +3,31 @@
 from scrapper import Scrapper
 import subprocess
 
-class WannaSleep():
-  def __init__():
-    self.home_dir = str(Path.home())
 
-  def get_error():
-    
+def get_error(command=None): 
+  from pathlib import Path
 
+  home_dir = str(Path.home())
+  error = ""
 
+  if command == None:
+    with open(f"{home_dir}/.bash_history", "r") as f:
+      command = f.readlines()[-1] #last command runned, error?
+      print(command)
+  try:
+    if " " in command:
+      output = subprocess.check_output(command.split(" "))
+    else:
+      output = subprocess.check_output(command.strip())
+  except subprocess.CalledProcessError as e:
+    error = e.output
+ 
+  return error
+
+if __name__ == "__main__":
+  error = get_error()
+
+  if len(error) <= 0:
+    print("No error was found")
+  else:
+    Scrapper(error)
